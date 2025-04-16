f187db6777dfade2a6d141af1c3a0f14e1e5e086
Tristan <Tristan@Ash.lan>
2025-04-16 11:35:20 +0200

    Unit Test

---
+++ test/assignment_1_prog5121_st10439502_tristanmills/Assignment_1_PROG5121_ST10439502_TristanMillsTest.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package assignment_1_prog5121_st10439502_tristanmills;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Tristan
+ */
+public class Assignment_1_PROG5121_ST10439502_TristanMillsTest {
+    
+    public Assignment_1_PROG5121_ST10439502_TristanMillsTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Assignment_1_PROG5121_ST10439502_TristanMills.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Assignment_1_PROG5121_ST10439502_TristanMills.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
